29539ea1e7e25da2d5c079bc3a032484688a715b
OHByeoul <suriessay@naver.com>
2019-04-01 12:24:33 +0900

    1. BoardController, BoardRequestMapping 작성 2. web.xml - servlet 추가

---
+++ src/com/revizio/moviebokka/controller/BoardController.java
@@ -0,0 +1,75 @@
+package com.revizio.moviebokka.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.revizio.moviebokka.constant.Constant;
+
+/**
+ * Servlet implementation class BoardController
+ */
+@WebServlet("/board/*")
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BoardRequestMapping boardRequestMapping;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardController() {
+        super();
+        boardRequestMapping = new BoardRequestMapping();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String contextPath = request.getContextPath();
+		String getUri = request.getRequestURI().substring(contextPath.length());
+		String [] route = getUri.split("/"); 
+		System.out.println(route[Constant.FIRST]+" "+route[Constant.SECOND]);
+		
+		String getView = "";
+		if(route.length<Constant.THIRD) {
+			boardRequestMapping.dispatcherRoute(route[Constant.FIRST],request,response);
+			getView = Route.getViewResolver(route[Constant.FIRST]);
+		} else {
+			boardRequestMapping.dispatcherRoute(route[Constant.SECOND],request,response);
+			System.out.println("route : "+Route.getDispathcerRoute(route[Constant.SECOND]));
+			System.out.println("view : "+Route.getViewResolver(route[Constant.SECOND]));		
+			getView = Route.getViewResolver(route[Constant.SECOND]);
+		}
+
+		if(!getView.equals(Route.JSON_FORMMAT)) {			
+			request.getRequestDispatcher(getView).forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/revizio/moviebokka/controller/BoardRequestMapping.java
@@ -0,0 +1,13 @@
+package com.revizio.moviebokka.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BoardRequestMapping implements RequestDispatcher{
+
+	@Override
+	public void dispatcherRoute(String route, HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+}
